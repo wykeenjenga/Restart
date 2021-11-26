b4a19a364c3d3b7c3350fdb90fc2a435304dd874
wykee2 <wycliffnjenga19@gmail.com>
2021-11-26 12:17:27 +0300

    Initial Commit

---
+++ Shared/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Shared
+//
+//  Created by Wykee Njenga on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
